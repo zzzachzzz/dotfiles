70d20ce9bbdba1f7132cdc21347420bed0bbe7f8
zzzachzzz <zach1rosen@gmail.com>
2020-02-02 15:45:45 -0700

    brew install jq

--- brew.sh
+++ brew.sh
@@ -2,6 +2,7 @@ brew install python
 brew install node
 brew install tree
 brew install ripgrep
+brew install jq
 brew install tmux
 brew install zsh
 brew install --HEAD universal-ctags/universal-ctags/universal-ctags
