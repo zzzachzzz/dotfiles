3bba84676ba01f7482f2c141f59ba788114ba4fc
zzzachzzz <zach1rosen@gmail.com>
2020-02-05 21:26:20 -0700

    Custom brew build for Vim, apt.sh for Linux dependencies

---
+++ apt.sh
@@ -0,0 +1,11 @@
+sudo apt-get install \
+  libncurses5-dev \
+  libgnome2-dev \
+  libgnomeui-dev \
+  libgtk2.0-dev \
+  libatk1.0-dev \
+  libbonoboui2-dev \
+  libcairo2-dev \
+  libx11-dev \
+  libxpm-dev \
+  libxt-dev
--- brew.sh
+++ brew.sh
@@ -6,4 +6,12 @@ brew install jq
 brew install tmux
 brew install zsh
 brew install --HEAD universal-ctags/universal-ctags/universal-ctags
+# In order to ensure +clipboard for Vim
+if ! brew list | grep -q "vim"; then
+  echo "Before installing vim, install dependencies from './apt.sh' (if on Linux),
+run 'brew edit vim', and add the options '--with-features=huge' and '--with-x'.
+Type 'Yes' to continue installation."
+  read REPLY
+  [[ $REPLY == "Yes" ]] && brew install --build-from-source vim
+fi
 
