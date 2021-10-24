c32123584517e36a51ef861a005954b64e7e695a
zzzachzzz <zach1rosen@gmail.com>
2021-10-24 15:17:37 -0600

    pacman.sh

---
+++ pacman.sh
@@ -0,0 +1,22 @@
+sudo pacman -Syu --noconfirm \
+  which \
+  man-db \
+  unzip \
+  binutils \
+  openssh \
+  git \
+  xclip \
+  tree \
+  jq \
+  zsh \
+  tmux \
+  ripgrep \
+  fzf \
+  ctags \
+  neovim \
+  python \
+  python-pip \
+  nodejs \
+  npm \
+  github-cli \
+
