cfe1fb0033ad63ed5953ca1b7bc215f6208b169f
zzzachzzz <zach1rosen@gmail.com>
2021-12-04 11:12:51 -0700

    Update symlinks for rename '~/github/' -> ~/git/'

--- symlink_commands.sh
+++ symlink_commands.sh
@@ -1,21 +1,25 @@
-ln -s ~/github/dotfiles/.zshrc ~/.zshrc
-ln -s ~/github/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/github/dotfiles/.vimrc ~/.vimrc
-ln -s ~/github/dotfiles/.vim/ ~/.vim
-ln -s ~/github/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
-ln -s ~/github/dotfiles/.ignore ~/.ignore
-ln -s ~/github/dotfiles/.gitignore_global ~/.gitignore_global
-ln -s ~/github/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/github/dotfiles/.pdbrc.py ~/.pdbrc.py
-ln -s ~/github/dotfiles/.vimrc_ide ~/github/dotfiles/sublime/.neovintageousrc
+ln -s ~/git/dotfiles/.zshrc ~/.zshrc
+ln -s ~/git/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/git/dotfiles/.vimrc ~/.vimrc
+ln -s ~/git/dotfiles/.vim/ ~/.vim
+ln -s ~/git/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
+ln -s ~/git/dotfiles/.ignore ~/.ignore
+ln -s ~/git/dotfiles/.gitignore_global ~/.gitignore_global
+ln -s ~/git/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/git/dotfiles/.pdbrc.py ~/.pdbrc.py
+ln -s ~/git/dotfiles/.vimrc_ide ~/git/dotfiles/sublime/.neovintageousrc
+
+# Linux {{{
+# ln -s ~/git/dotfiles/sublime/ ~/.config/sublime-text/Packages/User
+# }}}
 
 # OSX {{{
-# ln -s ~/github/dotfiles/vscode/ ~/Library/Application\ Support/Code/User
-# ln -s ~/github/dotfiles/sublime/ ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+# ln -s ~/git/dotfiles/vscode/ ~/Library/Application\ Support/Code/User
+# ln -s ~/git/dotfiles/sublime/ ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 # }}}
 
 # Windows symlinks for WSL (from cmd) {{{
-# mklink /D "%UserProfile%\AppData\Roaming\Code\User" "%UserProfile%\_\github\dotfiles\vscode"
-# mklink /D "%UserProfile%\AppData\Roaming\Sublime Text 3\Packages\User" "%UserProfile%\_\github\dotfiles\sublime"
+# mklink /D "%UserProfile%\AppData\Roaming\Code\User" "%UserProfile%\_\git\dotfiles\vscode"
+# mklink /D "%UserProfile%\AppData\Roaming\Sublime Text 3\Packages\User" "%UserProfile%\_\git\dotfiles\sublime"
 # }}}
 
