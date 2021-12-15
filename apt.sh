447254b2cf47cf3c2af5fb99310790a2f0bc261d
zzzachzzz <zach1rosen@gmail.com>
2021-12-15 11:33:32 -0700

    apt.sh for Debian

---
+++ apt.sh
@@ -0,0 +1,18 @@
+sudo apt update && sudo apt upgrade && sudo apt --yes install \
+  build-essential \
+  devscripts \
+  software-properties-common \
+  vim \
+  tree \
+  jq \
+  zsh \
+  tmux \
+  ripgrep \
+  fzf \
+  universal-ctags \
+  neovim \
+  python3 \
+  python3-pip \
+  nodejs \
+  npm \
+
