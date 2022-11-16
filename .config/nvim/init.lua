4b7b102c9d78ab064c76fcea0f69cb5576fae50f
zzzachzzz <zach1rosen@gmail.com>
2022-11-15 18:22:08 -0700

    Change deprecated cmp_nvim_lsp 'update_capabilities' -> 'default_capabilities'

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -170,7 +170,7 @@ do
     }, { buffer = bufnr })
   end
 
-  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
   local lspconfig = require 'lspconfig'
 
