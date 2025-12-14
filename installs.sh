594d311c68fa7885f1da3f454f57fed4609ecec9
zzzachzzz <zach1rosen@gmail.com>
2025-12-14 04:21:20 -0700

    Remove obsolete entries from `installs.sh`: OhMyZsh, vim-plug, NeoVim (nightly appimage)

--- installs.sh
+++ installs.sh
@@ -1,15 +1,5 @@
-# OhMyZsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 # zplug
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
-
-# NeoVim
-curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage -o ~/bin/nvim
-
-# vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # tmux-plugin-manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
